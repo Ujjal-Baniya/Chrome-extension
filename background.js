b689d03728fb5c1aa63383ec07e58f8aefbf8f45
UJJAL BANIYA <ujjalbaniya@gmail.com>
2020-10-12 20:49:24 +0545

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
-alert("HELLO MOREFUR")
-
-
 function MyGenericClick(info, tab){
     console.log("Clicked", info, tab)
 }
 
 function MyImageClick(info, tab){
     console.log("Image Clicked", info, tab)
+    chrome.windows.create({
+        "url":"www.facebook.com/sharer.php?u=" + info.url + "display=popup",
+        "type" : "popup"
+    })
 }
 
 chrome.contextMenus.create({
